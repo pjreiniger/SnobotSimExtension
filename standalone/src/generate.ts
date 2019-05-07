d736ef8036370036efc03faf32939689008f30fa
PJ Reiniger <pj.reiniger@gmail.com>
2019-05-06 22:42:24 -0400

    Starting to make standalone

--- standalone/src/generate.ts
+++ standalone/src/generate.ts
@@ -1,5 +1,119 @@
+import * as electron from 'electron';
+import * as fs from 'fs';
+import * as ncp from 'ncp';
+import * as path from 'path';
 
-function generateProjectButtonClick() {
-  projectFolder = (document.getElementById('projectFolder')).value;
-  console.log(projectFolder);
+
+const remote = electron.remote;
+const dialog = remote.dialog;
+
+function copyFile(source: string, dest: string) {
+    ncp.ncp(source, dest, {}, (err) => {
+        if (err) {
+            dialog.showMessageBox({
+                message: 'error: ' + err,
+                noLink: true,
+            });
+        }
+    });
+}
+
+
+function getGradleRioRegex() {
+  return /(id\s*?[\"|\']edu\.wpi\.first\.GradleRIO[\"|\'].*?version\s*?[\"|\'].+?[\"|\'])/g;
+}
+
+function getDefaultJavaDependenciesRegex() {
+  return /(\/\/ Defining my dependencies\. In this case, WPILib \(\+ friends\), and vendor libraries\.)/g;
+}
+
+function getCleanedJavaDependenciesRegex() {
+  return /(dependencies \{)/g;
+}
+
+function updateBuildFile(projectFolder: string) {
+    const buildFile = path.join(projectFolder, 'build.gradle');
+    const snobotSimPluginText = '\n    id "com.snobot.simulator.plugin.SnobotSimulatorPlugin" version "2019-3.0.0" apply false';
+    const snobotSimJavaApplyPlugin = '//////////////////////////////////\n// SnobotSim\n//////////////////////////////////\napply plugin: com.snobot.simulator.plugin.SnobotSimulatorPlugin\napply from: "snobotsim/snobot_sim.gradle"\n//////////////////////////////////\n\n';
+    const gradleBuildFile = fs.readFileSync(buildFile, 'utf8')
+    let newgFile = gradleBuildFile.replace(getGradleRioRegex(), `$1${snobotSimPluginText}`);
+
+    if (gradleBuildFile.search(getDefaultJavaDependenciesRegex()) !== -1) {
+        newgFile = newgFile.replace(getDefaultJavaDependenciesRegex(), `${snobotSimJavaApplyPlugin}$1`);
+    } else  {
+        newgFile = newgFile.replace(getCleanedJavaDependenciesRegex(), `${snobotSimJavaApplyPlugin}$1`);
+    }
+
+    fs.writeFile(buildFile, newgFile, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+function setupSnobotSimFiles(projectFolder : string, toFolder : string) {
+
+  fs.mkdirSync(toFolder);
+
+  // Quick error checking
+  const jsonFilePath = path.join(projectFolder, '.wpilib', 'wpilib_preferences.json');
+  if (!fs.existsSync(jsonFilePath)) {
+    dialog.showMessageBox({
+        message: 'This doesnt look like a WPILib project, bailing',
+        noLink: true,
+        });
+    return;
+  }
+
+  // Read the wpilib settings to figure out the project type
+  const wpilibConfig = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+  // Get the resource paths to copy
+  const basepath = electron.remote.app.getAppPath();
+  let resourceRoot = path.join(basepath, 'resources');
+  if (basepath.indexOf('default_app.asar') >= 0) {
+    resourceRoot = 'resources';
+  }
+
+  const snobotSimConfig = path.join(resourceRoot, 'SnobotSim.json');
+  let snobotSimBuildScript = "Invalid";
+  if(wpilibConfig.currentLanguage === "cpp") {
+      snobotSimBuildScript = path.join(resourceRoot, 'snobot_sim_cpp.gradle');
+  } else if(wpilibConfig.currentLanguage === "java") {
+      snobotSimBuildScript = path.join(resourceRoot, 'snobot_sim_java.gradle');
+  }
+
+  copyFile(snobotSimConfig, path.join(toFolder, 'SnobotSim.json'))
+  copyFile(snobotSimBuildScript, path.join(toFolder, 'snobot_sim.gradle'))
+
+  updateBuildFile(projectFolder)
+
+  dialog.showMessageBox({
+      message: 'Succesfully added SnobotSim to your project',
+      noLink: true,
+    });
+}
+
+function validateSnobotSimFiles() {
+
+  dialog.showMessageBox({
+      message: 'Validated SnobotSim installation',
+      noLink: true,
+    });
+}
+
+export function generateProjectButtonClick() {
+  const projectFolder = (document.getElementById('projectFolder') as HTMLInputElement).value;
+
+  if (!path.isAbsolute(projectFolder)) {
+    alert('Can only use an absolute path');
+    return;
+  }
+  const toFolder = path.join(projectFolder, 'snobotsim');
+
+  if (!fs.existsSync(toFolder)) {
+    setupSnobotSimFiles(projectFolder, toFolder)
+  } else {
+    validateSnobotSimFiles()
+  }
 }
---
+++ standalone/src/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: Electron.BrowserWindow | undefined;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    backgroundColor: '#2e2c29',
+    height: 250,
+    title: 'WPILib Utility',
+    width: 350,
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.setMenu(null);
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = undefined;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
