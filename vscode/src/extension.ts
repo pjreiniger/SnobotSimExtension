7d1b2032e13d0eaa9b7560f138667efc90ed7cb1
PJ Reiniger <pj.reiniger@gmail.com>
2019-05-17 21:29:38 -0400

    Mostly works

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -2,7 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { readFile } from './shared/file_utils';
+import { readFile, exists } from './shared/file_utils';
+import { setupSnobotSimFiles } from './shared/add_config';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -32,8 +33,41 @@ async function displayWebView(context: vscode.ExtensionContext, showOptions: vsc
         console.log("Got something " + data)
         console.log("Got something " + data.type)
 
-        if(data.type == "createProject") {
+        if(data.type === "createproject") {
+            
+        // use_vscode: (document.getElementById('use_vscode') as HTMLInputElement).checked,
+        // use_eclipse: (document.getElementById('use_eclipse') as HTMLInputElement).checked,
+        // use_intellij: (document.getElementById('use_intellij') as HTMLInputElement).value,
+        // projectFolder: (document.getElementById('projectFolder') as HTMLInputElement).value,
+        // robotName: (document.getElementById('robotName') as HTMLInputElement).value,
 
+            let addCustomSimulator = false
+            let useVsCode = data.data.use_vscode
+            let useEclipse = data.data.use_eclipse
+            let useIntellij = data.data.use_intellij
+            let basepath = context.extensionPath
+            let projectFolder = data.data.projectFolder
+            
+            const toFolder = path.join(projectFolder, 'snobotsim');
+            if(!await exists(toFolder))
+            {
+                const wpilibJsonPath = path.join(projectFolder, '.wpilib', 'wpilib_preferences.json');
+              if (!await exists(wpilibJsonPath)) {
+                console.log('not a wpilib thing....')
+                return;
+              }
+
+              // Read the wpilib settings to figure out the project type
+              const wpilibConfig = JSON.parse(await readFile(wpilibJsonPath));
+              await setupSnobotSimFiles(wpilibConfig, addCustomSimulator, useVsCode, useEclipse, useIntellij, basepath, projectFolder, toFolder)
+              vscode.window.showInformationMessage('Added SnobotSim configuration!');
+            }
+            else{
+        vscode.window.showWarningMessage('SnobotSim is already configured, any updates have to be made manually');
+            }
+        }
+        else {
+            console.log("Unknown option " + data.type + " - " + typeof(data.type))
         }
     });
 
